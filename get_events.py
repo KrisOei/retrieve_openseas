dd2b9d59d707d94ceb2b35fe13eb8bb776a88a9d
Kris Oei <oei.kris71@gmail.com>
2022-06-11 15:03:28 -0400

    get_events works

--- get_events.py
+++ get_events.py
@@ -5,42 +5,45 @@ from pymongo import MongoClient, mongo_client
 load_dotenv()
 API_KEY = os.getenv('API_KEY')
 
-contractAddress = '0x79FCDEF22feeD20eDDacbB2587640e45491b757f'
-eventType = 'successful'
-
 # Connect to databse
 client = MongoClient('mongodb://localhost:27017')
-db = client.mfers_collection
+db = client.mfers_events
 db.mfer
 
-# Setup Logging
+# logging
 logging.basicConfig(filename='tasks.log', level=logging.INFO)
 
-def get_event(token_id, dateTimeEpoch):
-    url = f"https://api.opensea.io/api/v1/events?asset_contract_address={contractAddress}&event_type={eventType}&occurred_before={dateTimeEpoch}"
+# We want to get all events within the following dates: 12/1/21 - 6/1/22, the API takes dates in Unix Epoch
+# The API also returns limited data so to get it all without exausting all requests we will need to use Cursor Pagination
 
-    headers = { 
+def get_events(cursor, assetContract = '0x79FCDEF22feeD20eDDacbB2587640e45491b757f', eventType = 'successful', startDate = '1654101626', endDate = '1638376826', **kwargs):
+    url = f"https://api.opensea.io/api/v1/events?asset_contract_address={assetContract}&event_type={eventType}&occurred_before={startDate}&occurred_after={endDate}&cursor={cursor}"
+
+    headers = {
     "Accept": "application/json",
     "X-API-KEY": API_KEY
     }
 
-    response = requests.get(url, headers=headers)
-    data = response.json()
-    return data
+    response = requests.get(url, headers=headers).text
+    response = json.loads(response)
+    return response
 
-# Start at 12/1/21
-def convertDate(year, month, day):
-    dateTimeEpoch = datetime.datetime(year, month, day).timestamp()
-    return dateTimeEpoch
+run = True
+next = ''
+result = list()
+i = 0
 
+while run:
+    response = get_events(cursor=next)
+    db.mfer.insert_one(response)
+    i += 1
 
-for i in range(0, 10):
+    print(f'Page {i} has been gotten')
+    logging.info(f'Task {i} has been completed at {datetime.datetime.now()}')
+
+    # Break Condition
+    if response['next'] is None:
+        run = False
+    else:
+        next = response['next']
     time.sleep(1)
-    token_id = i + 1
-    d = convertDate(2021, 11, 30)
-    data = get_event(token_id, d)
-    db.mfer.insert_one(data)
-
-    print(f'{token_id} tokens completed')
-    logging.info(f'{token_id} finished at {datetime.datetime.now()}')
-    
