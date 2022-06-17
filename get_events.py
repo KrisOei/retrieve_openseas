cc6cf2ff645c9745fe1b3aa7c39d7c2ba00d2a3c
Kris Oei <oei.kris71@gmail.com>
2022-06-16 22:49:07 -0400

    9

--- get_events.py
+++ get_events.py
@@ -13,11 +13,8 @@ db.mfer
 # logging
 logging.basicConfig(filename='tasks.log', level=logging.INFO)
 
-# We want to get all events within the following dates: 12/1/21 - 6/1/22, the API takes dates in Unix Epoch
-# The API also returns limited data so to get it all without exausting all requests we will need to use Cursor Pagination
-
-def get_events(cursor, assetContract = '0x79FCDEF22feeD20eDDacbB2587640e45491b757f', eventType = 'offer_entered', beforeDate = '1655237234', **kwargs):
-    url = f"https://api.opensea.io/api/v1/events?asset_contract_address={assetContract}&event_type={eventType}&occurred_before={beforeDate}&cursor={cursor}"
+def get_events(cursor, eventType, assetContract = '0x79FCDEF22feeD20eDDacbB2587640e45491b757f', beforeDate = '1655423813', **kwargs):
+    url = f"https://api.opensea.io/api/v1/events?asset_contract_address={assetContract}&event_type={eventType}&cursor={cursor}"
 
     headers = {
     "Accept": "application/json",
@@ -28,23 +25,33 @@ def get_events(cursor, assetContract = '0x79FCDEF22feeD20eDDacbB2587640e45491b75
     response = json.loads(response)
     return response
 
-run = True
-next = 'LWV2ZW50X3RpbWVzdGFtcD0yMDIyLTA2LTExKzAyJTNBMDglM0EzNi44MjQxNjkmLXBrPTY4MDMzOTU4Nzc='
-i = 0
+# Openseas event types excluding offers since the data is too much
+events = ['created', 'successful', 'cancelled', 'bid_entered', 'bid_withdrawn', 'transfer', 'approve']
 
-while run:
-    response = get_events(cursor=next)
-    db.mfer.insert_one(response)
-    i += 1
+# Gets data for event
+def get_data(ev):
+    run = True
+    next = ''
+    i = 0   
+    while run:
+        response = get_events(cursor=next, eventType=ev)
+        db.mfer.insert_one(response)
+        i += 1
 
-# Add page logger so that we know where we left off
-    print(f'Page {i} has been gotten')
-    logging.info(f'Task {next} has been completed at {datetime.datetime.now()}')
+    # Add page logger so that we know where we left off
+        print(f'Page {i} has been gotten')
+        logging.info(f'Task {next} has been completed at {datetime.datetime.now()}')
 
     # Break Condition
-    if response['next'] is None:
-        run = False
-        print(f'Completed at {datetime.datetime.now()}')
-    else:
-        next = response['next']
-    time.sleep(.33)
+        if response['next'] is None:
+            run = False
+            print(f'Task {ev} completed at {datetime.datetime.now()}')
+        else:
+            next = response['next']
+        time.sleep(.33)
+
+# Uses the get data function per event type
+for ev in events:
+    get_data(ev)
+
+print('All tasks complete')
